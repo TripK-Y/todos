3a52940ddb7bedb9d8f4fa426c260576dfb16003
kk60 <kelie.kan@gmail.com>
2021-08-04 09:28:39 -0500

    kk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { DataServiceComponent } from './services/data-service/data-service.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    TodosComponent,
+    DataServiceComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
